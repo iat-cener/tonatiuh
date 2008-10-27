3b49caa8f55bfa5842590bef46391178e45da7f1
amaiamutuberria <amaiamutuberria@5f0613ce-6e50-0410-beef-1d40ed1f67fe>
2008-10-27 15:50:30 +0000

    Changes due to NormalVector class.

--- trunk/TonatiuhProject/MaterialGeneric/src/MaterialGeneric.cpp
+++ trunk/TonatiuhProject/MaterialGeneric/src/MaterialGeneric.cpp
@@ -17,28 +17,29 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-Acknowledgments: 
+Acknowledgments:
 
-The development of Tonatiuh was started on 2004 by Dr. Manuel J. Blanco, 
-then Chair of the Department of Engineering of the University of Texas at 
-Brownsville. From May 2004 to July 2008, it was supported by the Department 
-of Energy (DOE) and the National Renewable Energy Laboratory (NREL) under 
-the Minority Research Associate (MURA) Program Subcontract ACQ-4-33623-06. 
-During 2007, NREL also contributed to the validation of Tonatiuh under the 
-framework of the Memorandum of Understanding signed with the Spanish 
-National Renewable Energy Centre (CENER) on February, 20, 2007 (MOU#NREL-07-117). 
-Since June 2006, the development of Tonatiuh is being led by the CENER, under the 
+The development of Tonatiuh was started on 2004 by Dr. Manuel J. Blanco,
+then Chair of the Department of Engineering of the University of Texas at
+Brownsville. From May 2004 to July 2008, it was supported by the Department
+of Energy (DOE) and the National Renewable Energy Laboratory (NREL) under
+the Minority Research Associate (MURA) Program Subcontract ACQ-4-33623-06.
+During 2007, NREL also contributed to the validation of Tonatiuh under the
+framework of the Memorandum of Understanding signed with the Spanish
+National Renewable Energy Centre (CENER) on February, 20, 2007 (MOU#NREL-07-117).
+Since June 2006, the development of Tonatiuh is being led by the CENER, under the
 direction of Dr. Blanco, now Director of CENER Solar Thermal Energy Department.
 
 Developers: Manuel J. Blanco (mblanco@cener.com), Amaia Mutuberria, Victor Martin.
 
-Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez, 
+Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez,
 Juana Amieva, Azael Mancillas, Cesar Cantu.
 ***************************************************************************/
 
 #include "MaterialGeneric.h"
+#include "NormalVector.h"
 #include "tgc.h"
-#include "tgf.h" 
+#include "tgf.h"
 #include "Trace.h"
 
 #include <QTime>
@@ -48,15 +49,15 @@ SO_NODE_SOURCE(MaterialGeneric);
 void MaterialGeneric::initClass()
 {
 	Trace trace( "MaterialGeneric::MaterialGeneric", false );
-	
+
 	 SO_NODE_INIT_CLASS( MaterialGeneric, TMaterial, "Material" );
 }
 
 MaterialGeneric::MaterialGeneric()
 {
 	Trace trace( "MaterialGeneric::MaterialGeneric", false );
-	
-	SO_NODE_CONSTRUCTOR( MaterialGeneric );	
+
+	SO_NODE_CONSTRUCTOR( MaterialGeneric );
 	SO_NODE_ADD_FIELD( m_reflectivity, (0.0) );
 	SO_NODE_ADD_FIELD( m_sigmaSlope, (2.0) );
 	SO_NODE_ADD_FIELD( m_sigmaSpecularity, (0.5) );
@@ -66,49 +67,49 @@ MaterialGeneric::MaterialGeneric()
 	SO_NODE_ADD_FIELD( m_emissiveColor, (0.0, 0.0, 0.0) );
 	SO_NODE_ADD_FIELD( m_shininess, (0.2) );
 	SO_NODE_ADD_FIELD( m_transparency, (0.0) );
-	
+
 	SO_NODE_DEFINE_ENUM_VALUE(Distribution, PILLBOX);
   	SO_NODE_DEFINE_ENUM_VALUE(Distribution, NORMAL);
-  	SO_NODE_SET_SF_ENUM_TYPE(m_distribution, Distribution); 
+  	SO_NODE_SET_SF_ENUM_TYPE(m_distribution, Distribution);
 	SO_NODE_ADD_FIELD( m_distribution, (PILLBOX) );
-	
+
 	m_sigmaOpt = tgf::computeSigmaOptica( m_sigmaSlope.getValue(), m_sigmaSpecularity.getValue() );
 
 	SoFieldSensor* m_reflectivitySensor = new SoFieldSensor( updateReflectivity, this );
-	m_reflectivitySensor->attach( &m_reflectivity );	
-				
+	m_reflectivitySensor->attach( &m_reflectivity );
+
 	m_sigmaSlopeSensor = new SoFieldSensor( updateSigmaOpt, this );
-	m_sigmaSlopeSensor->attach( &m_sigmaSlope );	
-	
+	m_sigmaSlopeSensor->attach( &m_sigmaSlope );
+
 	m_sigmaSpecularitySensor = new SoFieldSensor( updateSigmaOpt, this );
-	m_sigmaSpecularitySensor->attach( &m_sigmaSpecularity );	
-	
+	m_sigmaSpecularitySensor->attach( &m_sigmaSpecularity );
+
 	SoFieldSensor* m_ambientColorSensor = new SoFieldSensor( updateAmbientColor, this );
 	m_ambientColorSensor->attach( &m_ambientColor );
 	SoFieldSensor* m_diffuseColorSensor = new SoFieldSensor( updateDiffuseColor, this );
-	m_diffuseColorSensor->attach( &m_diffuseColor );	
+	m_diffuseColorSensor->attach( &m_diffuseColor );
 	SoFieldSensor* m_specularColorSensor = new SoFieldSensor( updateSpecularColor, this );
-	m_specularColorSensor->attach( &m_specularColor );	
+	m_specularColorSensor->attach( &m_specularColor );
 	SoFieldSensor* m_emissiveColorSensor = new SoFieldSensor( updateEmissiveColor, this );
-	m_emissiveColorSensor->attach( &m_emissiveColor );	
+	m_emissiveColorSensor->attach( &m_emissiveColor );
 	SoFieldSensor* m_shininessSensor = new SoFieldSensor( updateShininess, this );
-	m_shininessSensor->attach( &m_shininess );	
+	m_shininessSensor->attach( &m_shininess );
 	SoFieldSensor* m_transparencySensor = new SoFieldSensor( updateTransparency, this );
-	m_transparencySensor->attach( &m_transparency );		
+	m_transparencySensor->attach( &m_transparency );
 }
 
 MaterialGeneric::~MaterialGeneric()
-{	
+{
 	Trace trace( "MaterialGeneric::~MaterialGeneric", false );
 }
 
 QString MaterialGeneric::getIcon()
 {
 	Trace trace( "MaterialGeneric::getIcon", false );
-	
+
 	return QString(":icons/MaterialGeneric.png");
 }
- 
+
 void MaterialGeneric::updateSigmaOpt( void* data, SoSensor* )
 {
 	Trace trace( "MaterialGeneric::updateSigmaOpt", false );
@@ -119,7 +120,7 @@ void MaterialGeneric::updateSigmaOpt( void* data, SoSensor* )
 void MaterialGeneric::updateReflectivity( void* data, SoSensor* )
 {
 	Trace trace( "MaterialGeneric::updateReflectivity", false );
-	
+
 	MaterialGeneric* material = static_cast< MaterialGeneric* >( data );
 	if( material->m_reflectivity.getValue() < 0.0 ) material->m_reflectivity = 0.0;
    	if( material->m_reflectivity.getValue() > 1.0 ) material->m_reflectivity = 1.0;
@@ -128,7 +129,7 @@ void MaterialGeneric::updateReflectivity( void* data, SoSensor* )
 void MaterialGeneric::updateAmbientColor( void* data, SoSensor* )
 {
 	Trace trace( "MaterialGeneric::updateAmbientColor", false );
-	
+
    	MaterialGeneric* material = static_cast< MaterialGeneric* >( data );
  	material->ambientColor.setValue( material->m_ambientColor[0] );
 }
@@ -136,7 +137,7 @@ void MaterialGeneric::updateAmbientColor( void* data, SoSensor* )
 void MaterialGeneric::updateDiffuseColor( void* data, SoSensor* )
 {
 	Trace trace( "MaterialGeneric::updateDiffuseColor", false );
-	
+
    	MaterialGeneric* material = static_cast< MaterialGeneric* >( data );
  	material->diffuseColor.setValue( material->m_diffuseColor[0] );
 }
@@ -144,7 +145,7 @@ void MaterialGeneric::updateDiffuseColor( void* data, SoSensor* )
 void MaterialGeneric::updateSpecularColor( void* data, SoSensor* )
 {
 	Trace trace( "MaterialGeneric::updateSpecularColor", false );
-	
+
    	MaterialGeneric* material = static_cast< MaterialGeneric* >( data );
  	material->specularColor.setValue( material->m_specularColor[0] );
 }
@@ -152,7 +153,7 @@ void MaterialGeneric::updateSpecularColor( void* data, SoSensor* )
 void MaterialGeneric::updateEmissiveColor( void* data, SoSensor* )
 {
 	Trace trace( "MaterialGeneric::updateEmissiveColor", false );
-	
+
    	MaterialGeneric* material = static_cast< MaterialGeneric* >( data );
  	material->emissiveColor.setValue( material->m_emissiveColor[0] );
 }
@@ -160,7 +161,7 @@ void MaterialGeneric::updateEmissiveColor( void* data, SoSensor* )
 void MaterialGeneric::updateShininess( void* data, SoSensor* )
 {
 	Trace trace( "MaterialGeneric::updateShininess", false );
-	
+
    	MaterialGeneric* material = static_cast< MaterialGeneric* >( data );
  	material->shininess.setValue( material->m_shininess[0] );
 }
@@ -168,58 +169,58 @@ void MaterialGeneric::updateShininess( void* data, SoSensor* )
 void MaterialGeneric::updateTransparency( void* data, SoSensor* )
 {
 	Trace trace( "MaterialGeneric::updateTransparency", false );
-	
+
    	MaterialGeneric* material = static_cast< MaterialGeneric* >( data );
  	material->transparency.setValue( material->m_transparency[0] );
 }
-	
+
 Ray* MaterialGeneric::GetReflectedRay( const Ray& incident, DifferentialGeometry* dg, RandomDeviate& rand ) const
 {
 	Trace trace( "MaterialGeneric::GetReflectedRay", false );
-	
-	double randomNumber = rand.RandomDouble();	
+
+	double randomNumber = rand.RandomDouble();
 	if ( randomNumber >= m_reflectivity.getValue()  ) return 0;
-	
+
 	//Compute reflected ray (local coordinates )
 	Ray* reflected = new Ray();
 	reflected->origin = dg->point;
-	double cosTheta = DotProduct( dg->normal, incident.direction );	
-	reflected->direction = incident.direction - 2.0 * dg->normal * cosTheta; 
-	
+	double cosTheta = DotProduct( dg->normal, incident.direction );
+	reflected->direction = incident.direction - 2.0 * dg->normal * cosTheta;
+
 	double alfa = 0.0;
 	double ro = 0.0;
- 
+
 	if ( m_distribution.getValue() == 0 )
 	{
 		alfa = tgc::TwoPi * rand.RandomDouble();
-		ro = m_sigmaOpt * rand.RandomDouble(); 
+		ro = m_sigmaOpt * rand.RandomDouble();
 	 }
 	 else if (m_distribution.getValue() == 1 )
 	 {
 		alfa = tgc::Pi * rand.RandomDouble();
 		ro = m_sigmaOpt * tgf::AlternateBoxMuller( rand );
 	 }
-	 
+
 	//Compute unit vector s
 	Vector3D s = CrossProduct( dg->normal, reflected->direction );
 	Vector3D t;
 	if( s.Length() != 0 )
 	{
-		s = Normalize( s );
-		
+		s = Normalize( NormalVector( s ) );
+
 	 	//Compute unit vector t
-	 	t = Normalize( CrossProduct( s, dg->normal ) ); 
+	 	t = Normalize( NormalVector( CrossProduct( s, dg->normal ) ) );
 	}
 	else
 	{
-		//Si el rayo reflejado y la normal son colineales dpdu y dpdv son 
+		//Si el rayo reflejado y la normal son colineales dpdu y dpdv son
 		//perpendiculares al rayo reflejado
-		s = Normalize( dg->dpdu );
-		t = Normalize( dg->dpdv );	
-			
+		s = Normalize( NormalVector( dg->dpdu ) );
+		t = Normalize( NormalVector( dg->dpdv ) );
+
 	}
 
-	 reflected->direction = Normalize( reflected->direction );
-	 
-	 return reflected; 		
+	 reflected->direction = Normalize( NormalVector( reflected->direction ) );
+
+	 return reflected;
 }
