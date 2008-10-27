29f6d5bda9da9f24efeca3652d7c33cb80734c5d
amaiamutuberria <amaiamutuberria@5f0613ce-6e50-0410-beef-1d40ed1f67fe>
2008-10-27 17:02:21 +0000

    Normal class changed to NormalVector to compile with marble new version.

--- trunk/TonatiuhProject/ShapeParabolicDish/src/ShapeParabolicDisk.cpp
+++ trunk/TonatiuhProject/ShapeParabolicDish/src/ShapeParabolicDisk.cpp
@@ -17,22 +17,22 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-Acknowledgments: 
+Acknowledgments:
 
-The development of Tonatiuh was started on 2004 by Dr. Manuel J. Blanco, 
-then Chair of the Department of Engineering of the University of Texas at 
-Brownsville. From May 2004 to July 2008, it was supported by the Department 
-of Energy (DOE) and the National Renewable Energy Laboratory (NREL) under 
-the Minority Research Associate (MURA) Program Subcontract ACQ-4-33623-06. 
-During 2007, NREL also contributed to the validation of Tonatiuh under the 
-framework of the Memorandum of Understanding signed with the Spanish 
-National Renewable Energy Centre (CENER) on February, 20, 2007 (MOU#NREL-07-117). 
-Since June 2006, the development of Tonatiuh is being led by the CENER, under the 
+The development of Tonatiuh was started on 2004 by Dr. Manuel J. Blanco,
+then Chair of the Department of Engineering of the University of Texas at
+Brownsville. From May 2004 to July 2008, it was supported by the Department
+of Energy (DOE) and the National Renewable Energy Laboratory (NREL) under
+the Minority Research Associate (MURA) Program Subcontract ACQ-4-33623-06.
+During 2007, NREL also contributed to the validation of Tonatiuh under the
+framework of the Memorandum of Understanding signed with the Spanish
+National Renewable Energy Centre (CENER) on February, 20, 2007 (MOU#NREL-07-117).
+Since June 2006, the development of Tonatiuh is being led by the CENER, under the
 direction of Dr. Blanco, now Director of CENER Solar Thermal Energy Department.
 
 Developers: Manuel J. Blanco (mblanco@cener.com), Amaia Mutuberria, Victor Martin.
 
-Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez, 
+Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez,
 Juana Amieva, Azael Mancillas, Cesar Cantu.
 ***************************************************************************/
 
@@ -284,7 +284,7 @@ bool ShapeParabolicDisk::Intersect(const Ray& objectRay, double* tHit, Different
 	double E = DotProduct(dpdu, dpdu);
 	double F = DotProduct(dpdu, dpdv);
 	double G = DotProduct(dpdv, dpdv);
-	Vector3D N = Normalize(CrossProduct(dpdu, dpdv));
+	Vector3D N = Normalize( NormalVector( CrossProduct( dpdu, dpdv ) ) );
 	double e = DotProduct(N, d2Pduu);
 	double f = DotProduct(N, d2Pduv);
 	double g = DotProduct(N, d2Pdvv);
@@ -347,8 +347,8 @@ SbVec3f ShapeParabolicDisk::GetNormal (double u, double v) const
 	Point3D point = GetPoint3D( u, v );
 
 	Vector3D r( 0, 0, 1 );
-	Vector3D v1=Normalize(Vector3D( -point.x, -point.y, m_focus.getValue()-point.z ));
-	Vector3D normal=Normalize( r+v1 );
+	Vector3D v1=Normalize( NormalVector( -point.x, -point.y, m_focus.getValue()-point.z ));
+	Vector3D normal = Normalize( NormalVector( r + v1 ) );
 
 	return SbVec3f ( normal.x, normal.y, normal.z );
 
