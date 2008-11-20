6dc0407f93ff4780da0e8d657452eceee8c16099
amaiamutuberria <amutuberria@users.noreply.github.com>
2008-11-20 09:39:28 +0000

    commented code cleaned

--- TonatiuhProject/MaterialGeneric/src/MaterialGeneric.h
+++ TonatiuhProject/MaterialGeneric/src/MaterialGeneric.h
@@ -17,22 +17,22 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-Acknowledgments: 
+Acknowledgments:
 
-The development of Tonatiuh was started on 2004 by Dr. Manuel J. Blanco, 
-then Chair of the Department of Engineering of the University of Texas at 
-Brownsville. From May 2004 to July 2008, it was supported by the Department 
-of Energy (DOE) and the National Renewable Energy Laboratory (NREL) under 
-the Minority Research Associate (MURA) Program Subcontract ACQ-4-33623-06. 
-During 2007, NREL also contributed to the validation of Tonatiuh under the 
-framework of the Memorandum of Understanding signed with the Spanish 
-National Renewable Energy Centre (CENER) on February, 20, 2007 (MOU#NREL-07-117). 
-Since June 2006, the development of Tonatiuh is being led by the CENER, under the 
+The development of Tonatiuh was started on 2004 by Dr. Manuel J. Blanco,
+then Chair of the Department of Engineering of the University of Texas at
+Brownsville. From May 2004 to July 2008, it was supported by the Department
+of Energy (DOE) and the National Renewable Energy Laboratory (NREL) under
+the Minority Research Associate (MURA) Program Subcontract ACQ-4-33623-06.
+During 2007, NREL also contributed to the validation of Tonatiuh under the
+framework of the Memorandum of Understanding signed with the Spanish
+National Renewable Energy Centre (CENER) on February, 20, 2007 (MOU#NREL-07-117).
+Since June 2006, the development of Tonatiuh is being led by the CENER, under the
 direction of Dr. Blanco, now Director of CENER Solar Thermal Energy Department.
 
 Developers: Manuel J. Blanco (mblanco@cener.com), Amaia Mutuberria, Victor Martin.
 
-Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez, 
+Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez,
 Juana Amieva, Azael Mancillas, Cesar Cantu.
 ***************************************************************************/
 
@@ -41,8 +41,7 @@ Juana Amieva, Azael Mancillas, Cesar Cantu.
 
 #include <Inventor/fields/SoSFDouble.h>
 #include <Inventor/fields/SoSFEnum.h>
-#include <Inventor/fields/SoSFString.h> 
-#include <Inventor/sensors/SoFieldSensor.h>
+#include <Inventor/fields/SoSFString.h>
 
 #include "TMaterial.h"
 
@@ -50,47 +49,43 @@ Juana Amieva, Azael Mancillas, Cesar Cantu.
 class MaterialGeneric : public TMaterial
 {
 	SO_NODE_HEADER(MaterialGeneric);
-	
+
 public:
-	enum Distribution {                 
-		PILLBOX = 0,             
-		NORMAL = 1,               
+	enum Distribution {
+		PILLBOX = 0,
+		NORMAL = 1,
 	   };
 	MaterialGeneric( );
 	static void initClass();
-	
+
     QString getIcon();
-	Ray* GetReflectedRay( const Ray& incident, DifferentialGeometry* dg, RandomDeviate& rand  ) const; 
-	
+	Ray* GetReflectedRay( const Ray& incident, DifferentialGeometry* dg, RandomDeviate& rand  ) const;
+
 	SoSFDouble m_reflectivity;
 	SoSFDouble m_sigmaSlope;
-	SoSFDouble m_sigmaSpecularity;
+	//SoSFDouble m_sigmaSpecularity;
 	SoSFEnum m_distribution;
 	SoMFColor  m_ambientColor;
 	SoMFColor  m_diffuseColor;
 	SoMFColor  m_specularColor;
-	SoMFColor  m_emissiveColor; 
+	SoMFColor  m_emissiveColor;
 	SoMFFloat m_shininess;
 	SoMFFloat m_transparency ;
-	
-	 
+
+
 protected:
    	virtual ~MaterialGeneric();
-   	
+
    	double m_sigmaOpt;
-	
+
 	static void updateReflectivity( void* data, SoSensor* );
-	static void updateSigmaOpt( void* data, SoSensor* );
 	static void updateAmbientColor( void* data, SoSensor* );
 	static void updateDiffuseColor( void* data, SoSensor* );
 	static void updateSpecularColor( void* data, SoSensor* );
 	static void updateEmissiveColor( void* data, SoSensor* );
 	static void updateShininess( void* data, SoSensor* );
 	static void updateTransparency( void* data, SoSensor* );
-	
-	SoFieldSensor* m_sigmaSlopeSensor;
-	SoFieldSensor* m_sigmaSpecularitySensor;
-	
+
 };
 
 #endif /*MATERIALGENERIC_H_*/
