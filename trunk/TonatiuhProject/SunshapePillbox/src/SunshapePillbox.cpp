3c0b4ab73e08ad96498db748288344f1337775f1
amaiamutuberria <amaiamutuberria@5f0613ce-6e50-0410-beef-1d40ed1f67fe>
2008-11-27 16:48:40 +0000

    code review started: unused  #include guards deleted

--- trunk/TonatiuhProject/SunshapePillbox/src/SunshapePillbox.cpp
+++ trunk/TonatiuhProject/SunshapePillbox/src/SunshapePillbox.cpp
@@ -17,22 +17,22 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-Acknowledgments: 
+Acknowledgments:
 
-The development of Tonatiuh was started on 2004 by Dr. Manuel J. Blanco, 
-then Chair of the Department of Engineering of the University of Texas at 
-Brownsville. From May 2004 to July 2008, it was supported by the Department 
-of Energy (DOE) and the National Renewable Energy Laboratory (NREL) under 
-the Minority Research Associate (MURA) Program Subcontract ACQ-4-33623-06. 
-During 2007, NREL also contributed to the validation of Tonatiuh under the 
-framework of the Memorandum of Understanding signed with the Spanish 
-National Renewable Energy Centre (CENER) on February, 20, 2007 (MOU#NREL-07-117). 
-Since June 2006, the development of Tonatiuh is being led by the CENER, under the 
+The development of Tonatiuh was started on 2004 by Dr. Manuel J. Blanco,
+then Chair of the Department of Engineering of the University of Texas at
+Brownsville. From May 2004 to July 2008, it was supported by the Department
+of Energy (DOE) and the National Renewable Energy Laboratory (NREL) under
+the Minority Research Associate (MURA) Program Subcontract ACQ-4-33623-06.
+During 2007, NREL also contributed to the validation of Tonatiuh under the
+framework of the Memorandum of Understanding signed with the Spanish
+National Renewable Energy Centre (CENER) on February, 20, 2007 (MOU#NREL-07-117).
+Since June 2006, the development of Tonatiuh is being led by the CENER, under the
 direction of Dr. Blanco, now Director of CENER Solar Thermal Energy Department.
 
 Developers: Manuel J. Blanco (mblanco@cener.com), Amaia Mutuberria, Victor Martin.
 
-Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez, 
+Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez,
 Juana Amieva, Azael Mancillas, Cesar Cantu.
 ***************************************************************************/
 
@@ -45,18 +45,17 @@ SO_NODE_SOURCE(SunshapePillbox);
 void SunshapePillbox::initClass()
 {
 	Trace trace( "SunshapePillbox::initClass", false );
-	
 	SO_NODE_INIT_CLASS(SunshapePillbox, TSunShape, "TSunShape");
 }
 
 SunshapePillbox::SunshapePillbox( )
 {
 	Trace trace( "SunshapePillbox::SunshapePillbox", false );
-	
+
 	SO_NODE_CONSTRUCTOR(SunshapePillbox);
 	SO_NODE_ADD_FIELD(m_irradiance, (1000.0));
 	SO_NODE_ADD_FIELD(m_thetaMax, (0.00465));
-	
+
 }
 
 SunshapePillbox::~SunshapePillbox()
@@ -68,14 +67,14 @@ SunshapePillbox::~SunshapePillbox()
 void SunshapePillbox::generateRayDirection( Vector3D& direction, RandomDeviate& rand ) const
 {
 	Trace trace( "SunshapePillbox::generateRayDirection", false );
-	
+
     double phi = tgc::TwoPi * rand.RandomDouble();
     double theta = asin( sin( m_thetaMax.getValue() )*sqrt( rand.RandomDouble() ) );
     double sinTheta = sin( theta );
     double cosTheta = cos( theta );
     double cosPhi = cos( phi );
     double sinPhi = sin( phi );
-    
+
     direction.x = sinTheta*sinPhi;
     direction.y = -cosTheta;
     direction.z = sinTheta*cosPhi;
@@ -84,14 +83,14 @@ void SunshapePillbox::generateRayDirection( Vector3D& direction, RandomDeviate& 
 SoNode* SunshapePillbox::copy( SbBool copyConnections ) const
 {
 	Trace trace( "SunshapePillbox::copy", false );
-	
+
 	// Use the standard version of the copy method to create
 	// a copy of this instance, including its field data
 	SunshapePillbox* newSunShape = dynamic_cast< SunshapePillbox* >( SoNode::copy( copyConnections ) );
-	
+
 	// Copy the m_thetaMin, m_thetaMax private members explicitly
 	newSunShape->m_irradiance = m_irradiance;
 	newSunShape->m_thetaMax = m_thetaMax;
-	
+
 	return newSunShape;
 }
