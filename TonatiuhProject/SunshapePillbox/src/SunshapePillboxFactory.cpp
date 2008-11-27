8993ff7d7c19179e6aa8bde2189e5dbd16e406b2
amaiamutuberria <amutuberria@users.noreply.github.com>
2008-11-27 16:48:40 +0000

    code review started: unused  #include guards deleted

--- TonatiuhProject/SunshapePillbox/src/SunshapePillboxFactory.cpp
+++ TonatiuhProject/SunshapePillbox/src/SunshapePillboxFactory.cpp
@@ -17,22 +17,22 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-Acknowledgments: 
+Acknowledgments:
 
-The development of Tonatiuh was started on 2004 by Dr. Manuel J. Blanco, 
-then Chair of the Department of Engineering of the University of Texas at 
-Brownsville. From May 2004 to July 2008, it was supported by the Department 
-of Energy (DOE) and the National Renewable Energy Laboratory (NREL) under 
-the Minority Research Associate (MURA) Program Subcontract ACQ-4-33623-06. 
-During 2007, NREL also contributed to the validation of Tonatiuh under the 
-framework of the Memorandum of Understanding signed with the Spanish 
-National Renewable Energy Centre (CENER) on February, 20, 2007 (MOU#NREL-07-117). 
-Since June 2006, the development of Tonatiuh is being led by the CENER, under the 
+The development of Tonatiuh was started on 2004 by Dr. Manuel J. Blanco,
+then Chair of the Department of Engineering of the University of Texas at
+Brownsville. From May 2004 to July 2008, it was supported by the Department
+of Energy (DOE) and the National Renewable Energy Laboratory (NREL) under
+the Minority Research Associate (MURA) Program Subcontract ACQ-4-33623-06.
+During 2007, NREL also contributed to the validation of Tonatiuh under the
+framework of the Memorandum of Understanding signed with the Spanish
+National Renewable Energy Centre (CENER) on February, 20, 2007 (MOU#NREL-07-117).
+Since June 2006, the development of Tonatiuh is being led by the CENER, under the
 direction of Dr. Blanco, now Director of CENER Solar Thermal Energy Department.
 
 Developers: Manuel J. Blanco (mblanco@cener.com), Amaia Mutuberria, Victor Martin.
 
-Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez, 
+Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez,
 Juana Amieva, Azael Mancillas, Cesar Cantu.
 ***************************************************************************/
 
@@ -45,28 +45,26 @@ Juana Amieva, Azael Mancillas, Cesar Cantu.
 QString SunshapePillboxFactory::TSunShapeName() const
 {
 	Trace trace( "SunshapePillboxFactory::TSunShapeName", false );
-	
 	return QString( "Pillbox Sunshape" );
 }
 
-QIcon SunshapePillboxFactory::TSunShapeIcon() const 
+QIcon SunshapePillboxFactory::TSunShapeIcon() const
 {
 	Trace trace( "SunshapePillboxFactory::TSunShapeIcon", false );
-	
 	return QIcon( ":/icons/SunshapePillbox.png" );
 }
 
-SunshapePillbox* SunshapePillboxFactory::CreateTSunShape( ) const 
+SunshapePillbox* SunshapePillboxFactory::CreateTSunShape( ) const
 {
 	Trace trace( "SunshapePillboxFactory::CreateTSunShape", false );
-	
+
 	static bool firstTimeSunShape = true;
 	if ( firstTimeSunShape )
 	{
 	    SunshapePillbox::initClass();
 	    firstTimeSunShape = false;
 	}
-	
+
 	SunshapePillbox* pill = new SunshapePillbox;
 	return pill;
 }
