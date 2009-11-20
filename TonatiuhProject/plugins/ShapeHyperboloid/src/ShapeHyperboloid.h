44788d45b722b6ba9d488851c1ef98973655d505
amaiamutuberria <amutuberria@users.noreply.github.com>
2009-11-20 13:29:53 +0000

    ShapeHiperboloid activeSide removed

--- TonatiuhProject/plugins/ShapeHyperboloid/src/ShapeHyperboloid.h
+++ TonatiuhProject/plugins/ShapeHyperboloid/src/ShapeHyperboloid.h
@@ -62,15 +62,9 @@ public:
 
 	Point3D Sample( double u, double v ) const;
 
-	enum reverseOrientation {
-		INSIDE = 0,
-		OUTSIDE = 1,
-	   };
-
 	SoSFDouble focusLegth;
 	SoSFDouble distanceTwoFocus;
 	SoSFDouble reflectorMaxDiameter;
-	SoSFEnum activeSide;
 
 protected:
 	bool OutOfRange( double u, double v ) const;
