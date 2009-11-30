90ffe4ccec9321ef38ddcb78bdf7e3c8941c1043
manuel.jesus.blanco@gmail.com <manuel.jesus.blanco@gmail.com@5f0613ce-6e50-0410-beef-1d40ed1f67fe>
2009-11-30 00:43:18 +0000

    Modifications to eliminate all calls to Trace() in the members functions of Vector3D.

--- TonatiuhProject/src/Vector3D.cpp
+++ TonatiuhProject/src/Vector3D.cpp
@@ -32,7 +32,7 @@ direction of Dr. Blanco, now Director of CENER Solar Thermal Energy Department.
 
 Developers: Manuel J. Blanco (mblanco@cener.com), Amaia Mutuberria, Victor Martin.
 
-Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez,
+Contributors: Javier Garcia-Barberena, Iï¿½aki Perez, Inigo Pagola,  Gilda Jimenez,
 Juana Amieva, Azael Mancillas, Cesar Cantu.
 ***************************************************************************/
 
@@ -41,36 +41,29 @@ Juana Amieva, Azael Mancillas, Cesar Cantu.
 
 #include "NormalVector.h"
 #include "Point3D.h"
-#include "Trace.h"
 #include "Vector3D.h"
 
 Vector3D::Vector3D( double dx, double dy, double dz )
 : x(dx), y(dy), z(dz)
 {
-	Trace trace( "Vector3D::Vector3D", false );
 }
 
 Vector3D::Vector3D( const NormalVector& norm )
 : x(norm.x), y(norm.y), z(norm.z)
 {
-	Trace trace( "Vector3D::Vector3D", false );
 }
 
 Vector3D::Vector3D( Point3D point )
 : x(point.x), y(point.y), z(point.z)
 {
-	Trace trace( "Vector3D::Vector3D", false );
 }
 
 Vector3D::~Vector3D( )
 {
-	Trace trace( "Vector3D::Vector3D", false );
 }
 
 Vector3D& Vector3D::operator=( const Vector3D& vector )
 {
-	Trace trace( "Vector3D::operator=", false );
-
     if( this != &vector )
     {
         x = vector.x;
@@ -82,8 +75,6 @@ Vector3D& Vector3D::operator=( const Vector3D& vector )
 
 Vector3D& Vector3D::operator+=( const Vector3D& vector )
 {
-	Trace trace( "Vector3D::operator+=", false );
-
     x += vector.x;
     y += vector.y;
     z += vector.z;
@@ -92,8 +83,6 @@ Vector3D& Vector3D::operator+=( const Vector3D& vector )
 
 Vector3D& Vector3D::operator-=( const Vector3D& vector )
 {
-	Trace trace( "Vector3D::operator-=", false );
-
     x -= vector.x;
     y -= vector.y;
     z -= vector.z;
@@ -102,7 +91,6 @@ Vector3D& Vector3D::operator-=( const Vector3D& vector )
 
 Vector3D& Vector3D::operator*=( double scalar )
 {
-	Trace trace( "Vector3D::operator*=", false );
     x *= scalar;
     y *= scalar;
     z *= scalar;
@@ -111,8 +99,6 @@ Vector3D& Vector3D::operator*=( double scalar )
 
 Vector3D& Vector3D::operator/=( double scalar )
 {
-	Trace trace( "Vector3D::operator/=", false );
-
     double inv = 1.0/scalar;
     x *= inv;
     y *= inv;
@@ -122,8 +108,6 @@ Vector3D& Vector3D::operator/=( double scalar )
 
 void Vector3D::zero()
 {
-	Trace trace( "Vector3D::zero", false );
-
     x = 0.0;
     y = 0.0;
     z = 0.0;
@@ -131,8 +115,6 @@ void Vector3D::zero()
 
 bool Vector3D::operator==( const Vector3D& vector ) const
 {
-	Trace trace( "Vector3D::operator==", false );
-
 	if( this == &vector )
     	return true;
     else
@@ -144,126 +126,101 @@ bool Vector3D::operator==( const Vector3D& vector ) const
 
 bool Vector3D::operator!=( const Vector3D& vector ) const
 {
-	Trace trace( "Vector3D::operator!=", false );
-
 	if( this == &vector ) return false;
     else return ( x != vector.x ) || ( y != vector.y ) || ( z != vector.z );
 }
 
 double Vector3D::operator[]( int i ) const
 {
-	Trace trace( "Vector3D::operator[]", false );
-
     if( i == 0 ) return x;
     else return (i == 1) ? y : z;
 }
 
 double& Vector3D::operator[]( int i )
 {
-	Trace trace( "Vector3D::operator[]", false );
-
     if( i == 0 ) return x;
     else return (i == 1) ? y : z;
 }
 
 double Vector3D::LengthSquared( ) const
 {
-	Trace trace( "Vector3D::LengthSquared", false );
     return( x*x + y*y + z*z );
 }
 
 double Vector3D::Length( ) const
 {
-	Trace trace( "Vector3D::Length", false );
     return std::sqrt( LengthSquared( ) );
 }
 
 Vector3D operator-( const Vector3D& vector )
 {
-	Trace trace( "Vector3D operator+", false );
     return Vector3D( -vector.x, -vector.y, -vector.z );
 }
 
 Vector3D operator+( const Vector3D& lhs, const Vector3D& rhs )
 {
-	Trace trace( "Vector3D operator+", false );
     return Vector3D( lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z );
 }
 
 Vector3D operator-( const Vector3D& lhs, const Vector3D& rhs )
 {
-	Trace trace( "Vector3D operator-", false );
     return Vector3D( lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z );
 }
 
 Vector3D operator*( const Vector3D& vector, double scalar )
 {
-	Trace trace( "Vector3D operator*", false );
     return Vector3D( vector.x*scalar, vector.y*scalar, vector.z*scalar );
 }
 
 Vector3D operator*( double scalar, const Vector3D& vector )
 {
-	Trace trace( "Vector3D operator*", false );
     return Vector3D( vector.x*scalar, vector.y*scalar, vector.z*scalar );
 }
 
 Vector3D operator/( const Vector3D& vector, double scalar )
 {
-	Trace trace( "Vector3D operator/", false );
-
     double inv = 1.0/scalar;
     return Vector3D( vector.x*inv, vector.y*inv, vector.z*inv );
 }
 
 std::ostream& operator<<( std::ostream& os, const Vector3D& vector )
 {
-	Trace trace( "Vector3D operator<<", false );
-
     os << vector.x << ", " << vector.y << ", " << vector.z;
     return os;
 }
 
 double DotProduct( const Vector3D& vA, const Vector3D& vB )
 {
-	Trace trace( "Vector3D DotProduct", false );
     return vA.x*vB.x + vA.y*vB.y + vA.z*vB.z;
 }
 
 double DotProduct( const Vector3D& vA, const NormalVector& nB )
 {
-	Trace trace( "Vector3D DotProduct", false );
     return vA.x*nB.x + vA.y*nB.y + vA.z*nB.z;
 }
 
 double DotProduct( const NormalVector& nA, const Vector3D& vB )
 {
-	Trace trace( "Vector3D DotProduct", false );
     return nA.x*vB.x + nA.y*vB.y + nA.z*vB.z;
 }
 
 double AbsDotProduct( const Vector3D& vA, const Vector3D& vB )
 {
-	Trace trace( "Vector3D AbsDotProduct", false );
     return fabs( DotProduct( vA, vB ) );
 }
 
 double AbsDotProduct( const Vector3D& vA, const NormalVector& nB )
 {
-	Trace trace( "Vector3D AbsDotProduct", false );
     return fabs( DotProduct( vA, nB ) );
 }
 
 double AbsDotProduct( const NormalVector& nA, const Vector3D& vB )
 {
-	Trace trace( "Vector3D AbsDotProduct", false );
     return fabs( DotProduct( nA, vB ) );
 }
 
 Vector3D CrossProduct( const Vector3D& vA, const Vector3D& vB )
 {
-	Trace trace( "Vector3D CrossProduct", false );
-
     return Vector3D( ( vA.y*vB.z ) - ( vA.z*vB.y ),
                      ( vA.z*vB.x ) - ( vA.x*vB.z ),
                      ( vA.x*vB.y ) - ( vA.y*vB.x ) );
@@ -271,8 +228,6 @@ Vector3D CrossProduct( const Vector3D& vA, const Vector3D& vB )
 
 Vector3D CrossProduct( const Vector3D& vA, const NormalVector& nB )
 {
-	Trace trace( "Vector3D CrossProduct", false );
-
     return Vector3D( ( vA.y*nB.z ) - ( vA.z*nB.y ),
                      ( vA.z*nB.x ) - ( vA.x*nB.z ),
                      ( vA.x*nB.y ) - ( vA.y*nB.x ) );
@@ -280,8 +235,6 @@ Vector3D CrossProduct( const Vector3D& vA, const NormalVector& nB )
 
 Vector3D CrossProduct( const NormalVector& nA, const Vector3D& vB )
 {
-	Trace trace( "Vector3D CrossProduct", false );
-
     return Vector3D( ( nA.y*vB.z ) - ( nA.z*vB.y ),
                      ( nA.z*vB.x ) - ( nA.x*vB.z ),
                      ( nA.x*vB.y ) - ( nA.y*vB.x ) );
@@ -289,12 +242,10 @@ Vector3D CrossProduct( const NormalVector& nA, const Vector3D& vB )
 
 Vector3D Normalize( const Vector3D& vA )
 {
-	Trace trace( "Vector3D Normalize", false );
     return vA / vA.Length();
 }
 
 bool SameHemisphere( const Vector3D& vA, const Vector3D& vB )
 {
-	Trace trace( "Vector3D SameHemisphere", false );
 	return ( vA.z * vB.z > 0.0 );
 }
