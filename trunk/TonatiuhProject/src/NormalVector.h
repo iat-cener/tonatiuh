28f997a09618232c5fec232e8c4ab77463fa0fe3
manuel.jesus.blanco@gmail.com <manuel.jesus.blanco@gmail.com@5f0613ce-6e50-0410-beef-1d40ed1f67fe>
2009-12-28 22:02:59 +0000

    Changes to the Normal Vector class to implement the canonical forms of the addition and substraction operators as defined in Sutter and Alexandrescu "C++ Coding Standards. Topic 27".

--- trunk/TonatiuhProject/src/NormalVector.h
+++ trunk/TonatiuhProject/src/NormalVector.h
@@ -47,10 +47,9 @@ struct NormalVector
     NormalVector( double dx = 0.0, double dy = 0.0, double dz = 0.0 );
     explicit NormalVector( const Vector3D& vector );
     ~NormalVector( );
+
     NormalVector& operator+=( const NormalVector& nV);
-    NormalVector operator+( const NormalVector& nV ) const;
     NormalVector& operator-=( const NormalVector& nV );
-    NormalVector operator-( const NormalVector& nV ) const;
 
     NormalVector& operator*=( double scalar );
     NormalVector operator*( double scalar ) const;
@@ -72,6 +71,8 @@ struct NormalVector
     double z;
 };
 
+NormalVector operator+( NormalVector lhs, const NormalVector& rhs );
+NormalVector operator-( NormalVector lhs, const NormalVector& rhs );
 NormalVector operator*( double scalar, const NormalVector& nV );
 std::ostream& operator<<( std::ostream& os, const NormalVector& nV );
 double DotProduct( const NormalVector& nA, const NormalVector& nB );
