588cd0a8bc2cd865c823a8956c7caf06c65ad31c
amaiamutuberria <amaiamutuberria@5f0613ce-6e50-0410-beef-1d40ed1f67fe>
2010-04-23 09:19:28 +0000

    QtConcurrent::mappedReduced to parallelize ray tracer loop. Tonatiuh plugins added to project

---
+++ branches/Multi-Thread/Tonatiuh/plugins/SunshapePillbox/src/SunshapePillbox.cpp
@@ -0,0 +1,92 @@
+/***************************************************************************
+Copyright (C) 2008 by the Tonatiuh Software Development Team.
+
+This file is part of Tonatiuh.
+
+Tonatiuh program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Acknowledgments:
+
+The development of Tonatiuh was started on 2004 by Dr. Manuel J. Blanco,
+then Chair of the Department of Engineering of the University of Texas at
+Brownsville. From May 2004 to July 2008, it was supported by the Department
+of Energy (DOE) and the National Renewable Energy Laboratory (NREL) under
+the Minority Research Associate (MURA) Program Subcontract ACQ-4-33623-06.
+During 2007, NREL also contributed to the validation of Tonatiuh under the
+framework of the Memorandum of Understanding signed with the Spanish
+National Renewable Energy Centre (CENER) on February, 20, 2007 (MOU#NREL-07-117).
+Since June 2006, the development of Tonatiuh is being led by the CENER, under the
+direction of Dr. Blanco, now Director of CENER Solar Thermal Energy Department.
+
+Developers: Manuel J. Blanco (mblanco@cener.com), Amaia Mutuberria, Victor Martin.
+
+Contributors: Javier Garcia-Barberena, I�aki Perez, Inigo Pagola,  Gilda Jimenez,
+Juana Amieva, Azael Mancillas, Cesar Cantu.
+***************************************************************************/
+
+#include "SunshapePillbox.h"
+#include "tgc.h"
+
+SO_NODE_SOURCE(SunshapePillbox);
+
+void SunshapePillbox::initClass()
+{
+	SO_NODE_INIT_CLASS(SunshapePillbox, TSunShape, "TSunShape");
+}
+
+SunshapePillbox::SunshapePillbox( )
+{
+	SO_NODE_CONSTRUCTOR(SunshapePillbox);
+	SO_NODE_ADD_FIELD(m_irradiance, (1000.0));
+	SO_NODE_ADD_FIELD(m_thetaMax, (0.00465));
+
+}
+
+SunshapePillbox::~SunshapePillbox()
+{
+}
+
+//Light Interface
+void SunshapePillbox::generateRayDirection( Vector3D& direction, RandomDeviate& rand ) const
+{
+    double phi = tgc::TwoPi * rand.RandomDouble();
+    double theta = asin( sin( m_thetaMax.getValue() )*sqrt( rand.RandomDouble() ) );
+    double sinTheta = sin( theta );
+    double cosTheta = cos( theta );
+    double cosPhi = cos( phi );
+    double sinPhi = sin( phi );
+
+    direction.x = sinTheta*sinPhi;
+    direction.y = -cosTheta;
+    direction.z = sinTheta*cosPhi;
+}
+
+double SunshapePillbox::irradiance( void ) const
+{
+	return m_irradiance.getValue();
+}
+
+SoNode* SunshapePillbox::copy( SbBool copyConnections ) const
+{
+	// Use the standard version of the copy method to create
+	// a copy of this instance, including its field data
+	SunshapePillbox* newSunShape = dynamic_cast< SunshapePillbox* >( SoNode::copy( copyConnections ) );
+
+	// Copy the m_thetaMin, m_thetaMax private members explicitly
+	newSunShape->m_irradiance = m_irradiance;
+	newSunShape->m_thetaMax = m_thetaMax;
+
+	return newSunShape;
+}
