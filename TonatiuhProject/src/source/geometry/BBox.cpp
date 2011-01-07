437366a11ff4787a63ce63a6a5b5973cb2a2a58f
manuel.jesus.blanco <manuel.jesus.blanco@5f0613ce-6e50-0410-beef-1d40ed1f67fe>
2011-01-07 20:22:27 +0000

    Simplification of the Ray class. Now all Ray functions are inlined and defined in the class header file.

--- TonatiuhProject/src/source/geometry/BBox.cpp
+++ TonatiuhProject/src/source/geometry/BBox.cpp
@@ -48,7 +48,8 @@ Juana Amieva, Azael Mancillas, Cesar Cantu.
 
 
 BBox::BBox()
-: pMin( tgc::Infinity, tgc::Infinity, tgc::Infinity ), pMax( -tgc::Infinity, -tgc::Infinity, -tgc::Infinity )
+: pMin( tgc::Infinity, tgc::Infinity, tgc::Infinity ),
+  pMax( -tgc::Infinity, -tgc::Infinity, -tgc::Infinity )
 {
 }
 
@@ -59,60 +60,59 @@ BBox::BBox( const Point3D& point )
 
 BBox::BBox( const Point3D& point1, const Point3D& point2 )
 {
-
-        pMin = Point3D( std::min( point1.x, point2.x ),
-                                    std::min( point1.y, point2.y ),
-                                    std::min( point1.z, point2.z ) );
-        pMax = Point3D( std::max( point1.x, point2.x ),
-                                    std::max( point1.y, point2.y ),
-                                    std::max( point1.z, point2.z) );
+   pMin = Point3D( std::min( point1.x, point2.x ),
+                   std::min( point1.y, point2.y ),
+                   std::min( point1.z, point2.z ) );
+   pMax = Point3D( std::max( point1.x, point2.x ),
+                   std::max( point1.y, point2.y ),
+                   std::max( point1.z, point2.z) );
 }
 
 bool BBox::Overlaps( const BBox& bbox ) const
 {
-        return ( pMax.x >= bbox.pMin.x ) && ( pMin.x <= bbox.pMax.x ) &&
-               ( pMax.y >= bbox.pMin.y ) && ( pMin.y <= bbox.pMax.y ) &&
-               ( pMax.z >= bbox.pMin.z ) && ( pMin.z <= bbox.pMax.z);
+   return ( pMax.x >= bbox.pMin.x ) && ( pMin.x <= bbox.pMax.x ) &&
+          ( pMax.y >= bbox.pMin.y ) && ( pMin.y <= bbox.pMax.y ) &&
+          ( pMax.z >= bbox.pMin.z ) && ( pMin.z <= bbox.pMax.z);
 }
 
 bool BBox::Inside( const Point3D& point) const
 {
-        return ( point.x >= pMin.x && point.x <= pMax.x &&
-             point.y >= pMin.y && point.y <= pMax.y &&
-             point.z >= pMin.z && point.z <= pMax.z );
+   return ( point.x >= pMin.x && point.x <= pMax.x &&
+            point.y >= pMin.y && point.y <= pMax.y &&
+            point.z >= pMin.z && point.z <= pMax.z );
 }
 
 void BBox::Expand( double delta )
 {
-        pMin -= Vector3D( delta, delta, delta );
-        pMax += Vector3D( delta, delta, delta );
+   pMin -= Vector3D( delta, delta, delta );
+   pMax += Vector3D( delta, delta, delta );
 }
 
 double BBox::Volume() const
 {
-        Vector3D diagonal = pMax - pMin;
-        return diagonal.x * diagonal.y * diagonal.z;
+   Vector3D diagonal = pMax - pMin;
+   return diagonal.x * diagonal.y * diagonal.z;
 }
 
 int BBox::MaximumExtent() const
 {
-        Vector3D diagonal = pMax - pMin;
-        if( diagonal.x > diagonal.y && diagonal.x > diagonal.z) return 0;
-        else if( diagonal.y > diagonal.z) return 1;
-        else return 2;
+   Vector3D diagonal = pMax - pMin;
+   if( diagonal.x > diagonal.y && diagonal.x > diagonal.z) return 0;
+   else if( diagonal.y > diagonal.z) return 1;
+   else return 2;
 }
 
 void BBox::BoundingSphere( Point3D& center, double& radius ) const
 {
-    center = Point3D( (pMin.x+pMax.x)*0.5, (pMin.y+pMax.y)*0.5, (pMin.z+pMax.z)*0.5 );
-    radius = Distance( center, pMax );
+   center = Point3D( (pMin.x+pMax.x)*0.5, (pMin.y+pMax.y)*0.5, (pMin.z+pMax.z)*0.5 );
+   radius = Distance( center, pMax );
 }
 
 bool BBox::IntersectP( const Ray& ray, double* hitt0, double* hitt1 ) const
 {
     double t0 = ray.mint;
     double t1 = ray.maxt;
-	double tmin, tmax, tymin, tymax, tzmin, tzmax;
+    double tmin, tmax, tymin, tymax, tzmin, tzmax;
 
 	double divx = 1.0 / ray.direction.x;
 	if( divx >= 0.0 )
@@ -161,58 +161,37 @@ bool BBox::IntersectP( const Ray& ray, double* hitt0, double* hitt1 ) const
 	if ( tzmax < tmax ) tmax = tzmax;
 	if ( ( tmin < t1 ) && ( tmax > t0 ) )
 	{
-	    if( tmin < t0 ) tmin = t0;
-	    if( tmax > t1 ) tmax = t1;
-	    if( hitt0 ) *hitt0 = tmin;
-	    if( hitt1 ) *hitt1 = tmax;
-	    return true;
+		if( tmin < t0 ) tmin = t0;
+		if( tmax > t1 ) tmax = t1;
+		if( hitt0 ) *hitt0 = tmin;
+		if( hitt1 ) *hitt1 = tmax;
+		return true;
 	}
 	else return false;
 }
 
-/*
-bool BBox::IntersectP( const Ray& ray, double* hitt0, double* hitt1 ) const
-{
-    double t0 = ray.mint;
-    double t1 = ray.maxt;
-
-    for( int i = 0; i < 3; ++i )
-    {
-        double invRayDir = 1.0 / ray.direction[i];
-        double tNear = ( pMin[i] - ray.origin[i] ) * invRayDir;
-        double tFar = ( pMax[i] - ray.origin[i] ) * invRayDir;
-        if( tNear > tFar ) std::swap( tNear, tFar );
-        t0 = tNear > t0 ? tNear : t0;
-        t1 = tFar < t1 ? tFar : t1;
-        if( t0 > t1 ) return false;
-    }
-    if( hitt0 ) *hitt0 = t0;
-    if( hitt1 ) *hitt1 = t1;
-    return true;
-}
-*/
 BBox Union( const BBox& bbox, const Point3D& point )
 {
-	BBox result;
-	result.pMin.x = ( bbox.pMin.x < point.x ) ? bbox.pMin.x : point.x;
-	result.pMin.y = ( bbox.pMin.y < point.y ) ? bbox.pMin.y : point.y;
-	result.pMin.z = ( bbox.pMin.z < point.z ) ? bbox.pMin.z : point.z;
-	result.pMax.x = ( bbox.pMax.x > point.x ) ? bbox.pMax.x : point.x;
-	result.pMax.y = ( bbox.pMax.y > point.y ) ? bbox.pMax.y : point.y;
-	result.pMax.z = ( bbox.pMax.z > point.z ) ? bbox.pMax.z : point.z;
-	return result;
+   BBox unionBox;
+   unionBox.pMin.x = ( bbox.pMin.x < point.x ) ? bbox.pMin.x : point.x;
+   unionBox.pMin.y = ( bbox.pMin.y < point.y ) ? bbox.pMin.y : point.y;
+   unionBox.pMin.z = ( bbox.pMin.z < point.z ) ? bbox.pMin.z : point.z;
+   unionBox.pMax.x = ( bbox.pMax.x > point.x ) ? bbox.pMax.x : point.x;
+   unionBox.pMax.y = ( bbox.pMax.y > point.y ) ? bbox.pMax.y : point.y;
+   unionBox.pMax.z = ( bbox.pMax.z > point.z ) ? bbox.pMax.z : point.z;
+   return unionBox;
 }
 
 BBox Union( const BBox& bbox1, const BBox& bbox2 )
 {
-	BBox result;
-	result.pMin.x = ( bbox1.pMin.x < bbox2.pMin.x ) ? bbox1.pMin.x : bbox2.pMin.x;
-	result.pMin.y = ( bbox1.pMin.y < bbox2.pMin.y ) ? bbox1.pMin.y : bbox2.pMin.y;
-	result.pMin.z = ( bbox1.pMin.z < bbox2.pMin.z ) ? bbox1.pMin.z : bbox2.pMin.z;
-	result.pMax.x = ( bbox1.pMax.x > bbox2.pMax.x ) ? bbox1.pMax.x : bbox2.pMax.x;
-	result.pMax.y = ( bbox1.pMax.y > bbox2.pMax.y ) ? bbox1.pMax.y : bbox2.pMax.y;
-	result.pMax.z = ( bbox1.pMax.z > bbox2.pMax.z ) ? bbox1.pMax.z : bbox2.pMax.z;
-	return result;
+   BBox unionBox;
+   unionBox.pMin.x = ( bbox1.pMin.x < bbox2.pMin.x ) ? bbox1.pMin.x : bbox2.pMin.x;
+   unionBox.pMin.y = ( bbox1.pMin.y < bbox2.pMin.y ) ? bbox1.pMin.y : bbox2.pMin.y;
+   unionBox.pMin.z = ( bbox1.pMin.z < bbox2.pMin.z ) ? bbox1.pMin.z : bbox2.pMin.z;
+   unionBox.pMax.x = ( bbox1.pMax.x > bbox2.pMax.x ) ? bbox1.pMax.x : bbox2.pMax.x;
+   unionBox.pMax.y = ( bbox1.pMax.y > bbox2.pMax.y ) ? bbox1.pMax.y : bbox2.pMax.y;
+   unionBox.pMax.z = ( bbox1.pMax.z > bbox2.pMax.z ) ? bbox1.pMax.z : bbox2.pMax.z;
+   return unionBox;
 }
 
 std::ostream& operator<<( std::ostream& os, const BBox& bbox )
