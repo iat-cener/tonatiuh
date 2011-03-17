902ffaeed0405d4cbdee8fd991e060f0838d271a
amaiamutuberria <amaiamutuberria@5f0613ce-6e50-0410-beef-1d40ed1f67fe>
2011-03-17 14:55:33 +0000

    Linux structure created for 1.1.6

---
+++ updates/release-1.1.6/linux/bin/release/plugins/ShapeBezierSurface/strip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+strip -s -x ShapeSphericalRectangle/lib*
+strip -s -x ShapeCylinder/lib*
+strip -s -x ShapeSphericalPolygon/lib*
+strip -s -x ShapeCone/lib*
+strip -s -x SunshapePillbox/lib*
+strip -s -x ShapeSphere/lib*
+strip -s -x ShapeBezierSurface/lib*
+strip -s -x SunshapeBuie/lib*
+strip -s -x ShapeParabolicRectangle/lib*
+strip -s -x RandomRngStream/lib*
+strip -s -x ShapeTrumpet/lib*
+strip -s -x ShapeParabolicDish/lib*
+strip -s -x RandomMersenneTwister/lib*
+strip -s -x ShapeTroughParabola/lib*
+strip -s -x ShapeHyperboloid/lib*
+strip -s -x PhotonMapDefault/lib*
+strip -s -x ShapeTroughHyperbola/lib*
+strip -s -x ShapeFlatTriangle/lib*
+strip -s -x  MaterialStandardSpecular/lib*
+strip -s -x ShapeTroughCPC/lib*
+strip -s -x ShapeFlatRectangle/lib*
+strip -s -x MaterialBasicRefractive/lib*
+strip -s -x ShapeTroughCHC/lib*
+strip -s -x ShapeFlatDisk/lib*
+strip -s -x TrackerHeliostat/lib*
+strip -s -x TrackerLinearFresnel/lib*
+strip -s -x TrackerOneAxis/lib*
+
