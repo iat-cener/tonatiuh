dbb4c8e0bb9e17f3fb9039c5f6c360277a077802
amaiamutuberria <amutuberria@users.noreply.github.com>
2011-04-05 10:10:22 +0000

    The light has been changed to trace rays only from the scene surfaces projection areas.

--- TonatiuhProject/plugins/SunshapePillbox/src/SunshapePillbox.h
+++ TonatiuhProject/plugins/SunshapePillbox/src/SunshapePillbox.h
@@ -58,6 +58,7 @@ public:
     //Sunshape Interface
     void GenerateRayDirection( Vector3D& direction, RandomDeviate& rand) const;
 	double GetIrradiance() const;
+    double GetThetaMax() const;
 
 	trt::TONATIUH_REAL irradiance;
 	trt::TONATIUH_REAL thetaMax;
