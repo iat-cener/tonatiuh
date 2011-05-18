0ba8dac2af847e99d38346c2b6741ceff7ce11e4
Dawann <Dawann@5f0613ce-6e50-0410-beef-1d40ed1f67fe>
2011-05-18 10:49:07 +0000

    To not create automaticaly bad empty object

--- trunk/TonatiuhProject/plugins/TrackerOneAxis/src/TrackerOneAxis.cpp
+++ trunk/TonatiuhProject/plugins/TrackerOneAxis/src/TrackerOneAxis.cpp
@@ -91,10 +91,10 @@ void TrackerOneAxis::evaluate()
 	if( !m_azimuth.isConnected() ) return;
 	if( !m_zenith.isConnected() ) return;
 
-	TSeparatorKit* sunNode = static_cast< TSeparatorKit* > ( m_scene->getPart( "childList[0]", true ) );
+	TSeparatorKit* sunNode = static_cast< TSeparatorKit* > ( m_scene->getPart( "childList[0]", false ) );
 	if( !sunNode )	return;
 
-	TSeparatorKit* rootNode = static_cast< TSeparatorKit* > ( sunNode->getPart( "childList[0]", true ) );
+	TSeparatorKit* rootNode = static_cast< TSeparatorKit* > ( sunNode->getPart( "childList[0]", false ) );
 	if( !rootNode )	return;
 
 	SoSearchAction* coinSearch = new SoSearchAction();
