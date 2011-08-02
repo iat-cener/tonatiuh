e639b90843ec9385af1ccf5dab2549f48d03b14b
amaiamutuberria <amutuberria@users.noreply.github.com>
2011-08-02 15:43:57 +0000

    Components parts added to main window

---
+++ TonatiuhProject/src/source/gui/raytracedialog.h
@@ -0,0 +1,8 @@
+/****************************************************************************
+** Form interface generated from reading ui file 'source/gui/raytracedialog.ui'
+**
+** Created: sáb may 7 12:29:10 2011
+**
+** WARNING! All changes made in this file will be lost!
+****************************************************************************/
+
