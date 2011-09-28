f9f692a4a0dcfc01c84b0eeb1a8eb6d9652c054e
amaiamutuberria <amaiamutuberria@5f0613ce-6e50-0410-beef-1d40ed1f67fe>
2011-09-28 11:34:16 +0000

    Error solved into Linear Fresnel plugin name

--- trunk/TonatiuhProject/plugins/TrackerLinearFresnel/src/TrackerLinearFresnelFactory.cpp
+++ trunk/TonatiuhProject/plugins/TrackerLinearFresnel/src/TrackerLinearFresnelFactory.cpp
@@ -32,7 +32,7 @@ direction of Dr. Blanco, now Director of CENER Solar Thermal Energy Department.
 
 Developers: Manuel J. Blanco (mblanco@cener.com), Amaia Mutuberria, Victor Martin.
 
-Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez,
+Contributors: Javier Garcia-Barberena, Iï¿½aki Perez, Inigo Pagola,  Gilda Jimenez,
 Juana Amieva, Azael Mancillas, Cesar Cantu.
 ***************************************************************************/
 
@@ -43,7 +43,7 @@ Juana Amieva, Azael Mancillas, Cesar Cantu.
 
 QString TrackerLinearFresnelFactory::TTrackerName() const
 {
-	return QString("Heliostat_tracker");
+	return QString("Linear_Fresnel_tracker");
 }
 
 QIcon TrackerLinearFresnelFactory::TTrackerIcon() const
