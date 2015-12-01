432f7c6fa9d4d4ee3768b60ff4a8a80c65b1148a
amutuberria <amutuberria@users.noreply.github.com>
2015-12-01 11:39:27 +0100

    Comments removed

--- TonatiuhProject/fields/UserMField.cpp
+++ TonatiuhProject/fields/UserMField.cpp
@@ -5,7 +5,6 @@
  *      Author: amutuberria
  */
 
-#include <iostream>
 #include "UserMField.h"
 
 SoType UserMField::classTypeId STATIC_SOTYPE_INIT;
@@ -26,10 +25,6 @@ UserMField::~UserMField()
 
 void UserMField::initClass(void)
 {
-	std::cout<<"UserMField::inherited: "<<inherited::getClassTypeId().getName().getString()<<std::endl;
-	std::cout<<"UserMField::inherited 2 : "<<UserMField::getClassTypeId().getName().getString()<<std::endl;
-	std::cout<<"UserMField::inherited 3 : "<<UserMField::getClassTypeId().getKey()<<std::endl;
-	//std::cout<<"UserMField::inherited: "<<SoType::badType().getClassTypeId().getName().getString()<<std::endl;
 	PRIVATE_FIELD_INIT_CLASS(UserMField, "UserMField", inherited, NULL);
 }
 
