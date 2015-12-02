3dcdddae27eb517f894513f12a9c4d75cc2e30c1
amutuberria <amutuberria@users.noreply.github.com>
2015-12-02 14:52:03 +0100

    Changes to run in script mode

--- TonatiuhProject/src/source/application/main.cpp
+++ TonatiuhProject/src/source/application/main.cpp
@@ -71,6 +71,13 @@ Juana Amieva, Azael Mancillas, Cesar Cantu.
 #include "UserMField.h"
 #include "UserSField.h"
 
+
+#include <QScriptEngine>
+#include <QTextStream>
+#include "PluginManager.h"
+#include "ScriptRayTracer.h"
+#include "tonatiuh_script.h"
+
 /*!
   \mainpage
 The Tonatiuh project aims to create an open source, cutting-edge, accurate, and easy to use Monte Carlo ray tracer for the optical simulation of solar concentrating systems. It intends to advance the state-of-the-art of the simulation tools available for the design and analysis of solar concentrating systems, and to make those tools freely available to anyone interested in using and improving them. Some of the most relevant design goals of Tonatiuh are:
@@ -141,7 +148,6 @@ int main( int argc, char ** argv )
 	TDefaultTransmissivity::initClass();
 
 
-	MainWindow* mw;
 	splash->showMessage( QObject::tr("Setting up the main window..."), topRight, Qt::black );
 
     int exit;
@@ -153,43 +159,89 @@ int main( int argc, char ** argv )
     	if( fileInfo.completeSuffix() == QLatin1String( "tnhs") )
     	{
 
-       		mw = new MainWindow("");
-       		mw->show();
-       	    splash->finish( mw );
-       	    delete splash;
-    	    //exit = a.exec();
-       		QTime dieTime = QTime::currentTime().addMSecs( 100 );
-       	    while( QTime::currentTime() < dieTime )
-       	    {
-       	        QCoreApplication::processEvents( QEventLoop::AllEvents, 100 );
-       	    }
-       		mw->ExecuteScriptFile( tonatiuhFile );
-       		mw->Clear();
-       		mw->close();
-       		exit = 0;
+    		QDir pluginsDirectory( qApp->applicationDirPath() );
+    		pluginsDirectory.cd( "plugins" );
+
+    		PluginManager pluginManager;
+    		pluginManager.LoadAvailablePlugins( pluginsDirectory );
+
+
+    		QString fileName( argv[1] );
+    		QFileInfo fileInfo( fileName );
+
+    		QDir testDirectory( fileInfo.absolutePath() );
+    		testDirectory.cd( "." );
+
+    		QScriptEngine* interpreter = new QScriptEngine;
+
+    		QScriptValue tonatiuh = interpreter->newQObject( new MainWindow( QLatin1String("") ) );
+    		interpreter->globalObject().setProperty( "tonatiuh", tonatiuh );
+
+
+    		QFile scriptFile( fileName );
+    		if( !scriptFile.open( QIODevice::ReadOnly) )
+    		{
+    			QString errorMessage = QString( "Cannot open file %1." ).arg( fileName );
+    			std::cerr<<errorMessage.toStdString()<<std::endl;
+    		}
+
+
+    		QTextStream in( &scriptFile );
+    		QString program = in.readAll();
+    		scriptFile.close();
+
+    	   	std::cout<<"error 1"<<std::endl;
+
+
+    		QScriptSyntaxCheckResult checkResult = interpreter->checkSyntax( program );
+    		if( checkResult.state() != QScriptSyntaxCheckResult::Valid )
+    		{
+    			QString errorMessage = QString( "Script Syntaxis Error.\n"
+    					"Line: %1. %2" ).arg( QString::number( checkResult.errorLineNumber() ), checkResult.errorMessage () );
+    			std::cerr<<errorMessage.toStdString()<<std::endl;
+    			return -1;
+    		}
+
+    	   	std::cout<<"error 2"<<std::endl;
+    		QScriptValue result = interpreter->evaluate( program );
+    		if( result.isError () )
+    		{
+    			QScriptValue lineNumber = result.property( "lineNumber");
+
+    			QString errorMessage = QString( "Script Execution Error.\nLine %1. %2" ).arg( QString::number( lineNumber.toNumber() ), result.toString() );
+    			std::cerr<<errorMessage.toStdString()<<std::endl;
+    			return -1;
+
+    		}
+
+    		exit = 0;
     	}
     	else
     	{
-    		mw = new MainWindow( tonatiuhFile );
+
+    		MainWindow* mw = new MainWindow( tonatiuhFile );
 
        		mw->show();
        	    splash->finish( mw );
        	    delete splash;
     	    exit = a.exec();
+       		delete mw;
     	}
    	}
    	else
    	{
-   		mw = new MainWindow("");
+   		MainWindow* mw = new MainWindow("");
    		mw->show();
    	    splash->finish( mw );
    	    delete splash;
 	    exit = a.exec();
+   		delete mw;
 
    	}
 
 
-    delete mw;
+   	std::cout<<"error"<<std::endl;
+
 
 	return exit;
 }
