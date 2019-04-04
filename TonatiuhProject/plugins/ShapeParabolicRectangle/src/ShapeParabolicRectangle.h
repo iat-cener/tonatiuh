2374d17ef53764b99c43e8f03592254de15c0f0e
amutuberria <amutuberria@users.noreply.github.com>
2019-04-04 15:39:52 +0200

    Rolled back to r1356

--- TonatiuhProject/plugins/ShapeParabolicRectangle/src/ShapeParabolicRectangle.h
+++ TonatiuhProject/plugins/ShapeParabolicRectangle/src/ShapeParabolicRectangle.h
@@ -15,6 +15,25 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Acknowledgments:
+
+The development of Tonatiuh was started on 2004 by Dr. Manuel J. Blanco,
+then Chair of the Department of Engineering of the University of Texas at
+Brownsville. From May 2004 to July 2008, it was supported by the Department
+of Energy (DOE) and the National Renewable Energy Laboratory (NREL) under
+the Minority Research Associate (MURA) Program Subcontract ACQ-4-33623-06.
+During 2007, NREL also contributed to the validation of Tonatiuh under the
+framework of the Memorandum of Understanding signed with the Spanish
+National Renewable Energy Centre (CENER) on February, 20, 2007 (MOU#NREL-07-117).
+Since June 2006, the development of Tonatiuh is being led by the CENER, under the
+direction of Dr. Blanco, now Director of CENER Solar Thermal Energy Department.
+
+Developers: Manuel J. Blanco (mblanco@cener.com), Amaia Mutuberria, Victor Martin.
+
+Contributors: Javier Garcia-Barberena, Iñaki Perez, Inigo Pagola,  Gilda Jimenez,
+Juana Amieva, Azael Mancillas, Cesar Cantu.
 ***************************************************************************/
 
 #ifndef SHAPEPARABOLICRECTANGLE_H_
